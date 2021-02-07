29205d28644f55936c71d26d58e786b049fe1cb6
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-02-07 21:08:12 +0100

    Navbar Component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,21 +1,19 @@
 import Link from "next/link";
 import { StyledUl } from "./Navbar.style";
 
-const Navbar = () => {
-  return (
-    <StyledUl>
-      <li>
-        <Link href='/'>
-          <a> Home </a>
-        </Link>
-      </li>
-      <li>
-        <Link href='/about'>
-          <a> About </a>
-        </Link>
-      </li>
-    </StyledUl>
-  );
-};
+const Navbar = () => (
+  <StyledUl>
+    <li>
+      <Link href='/'>
+        <a> Home </a>
+      </Link>
+    </li>
+    <li>
+      <Link href='/about'>
+        <a> About </a>
+      </Link>
+    </li>
+  </StyledUl>
+);
 
 export default Navbar;
